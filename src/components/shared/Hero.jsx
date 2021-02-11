df63ed53d2125df93ae84ab480a57247bd21b675
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2021-02-11 18:21:02 -0500

    Clear error for badge img

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Img from "gatsby-image";
 import Rotate from "react-reveal/Rotate";
 
-function Hero({ img, img2 }) {
+function Hero(props) {
 	return (
 		<div className="w-full h-auto" style={{ position: `relative` }}>
-			<Img alt="Banner" fluid={img} />
+			<Img alt="Banner" fluid={props.img} />
 			<div className="absolute bottom-0 left-0 z-10 w-16 h-16 mb-1 ml-1 md:pb-2 md:pl-2 md:w-36 lg:w-48 md:h-36 lg:h-48">
-				<Rotate top left>
-					<Img
-						alt="MBE (Minority Business Enterprise) Certification"
-						fluid={img2}
-					/>
-				</Rotate>
+				{props.img2 && (
+					<Rotate top left>
+						<Img
+							alt="MBE (Minority Business Enterprise) Certification"
+							fluid={props.img2}
+						/>
+					</Rotate>
+				)}
 			</div>
 		</div>
 	);

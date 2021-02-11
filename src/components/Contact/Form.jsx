c4c21c03ae35c49047f59c38daa50b928bcf4744
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2021-02-11 18:32:51 -0500

    Fix encode function

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -14,7 +14,7 @@ export default function ContactForm() {
 	const encode = (data) => {
 		return Object.keys(data)
 			.map(
-				(key) => encodeURIComponent(key) + "+" + encodeURIComponent(data[key])
+				(key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
 			)
 			.join("&");
 	};

66d79e8d2d7de75383ef17cd8a4423f91dd734e4
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2021-02-15 14:56:08 -0500

    Changed MBE img & minor adjustments

--- src/components/Home/Info.jsx
+++ src/components/Home/Info.jsx
@@ -28,7 +28,7 @@ const INFO = [
 const renderInfo = () => {
 	return INFO.map((item, index) => (
 		<div
-			className="flex-col items-center justify-between w-full h-full px-6 py-10 m-4 text-white bg-gray-600 border border-white rounded shadow-white md:mx-4"
+			className="flex flex-col items-center justify-between w-full h-full px-6 py-10 m-4 text-white bg-gray-600 border border-white rounded shadow-white md:mx-4"
 			key={index}
 		>
 			<img src={item.image} alt={item.title} />
